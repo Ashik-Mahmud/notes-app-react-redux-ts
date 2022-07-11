0d12369b3bb687fe2f53bd651806fddf6d8f0a87
ashikmahmud <ashikmamud187@gmail.com>
2022-07-12 01:58:33 +0600

    setting up redux typescript react projects called notes application

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppContainer className="App">
+      <h1>Hello World</h1>
+    </AppContainer>
   );
 }
 
+const AppContainer = styled.section`
+  position: relative;
+  max-width: min(1250px, 100% - 2rem);
+  margin-inline: auto;
+`;
 export default App;
